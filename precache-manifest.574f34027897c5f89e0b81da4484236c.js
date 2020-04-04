c23d75ea4ddd14b4494899eb2c55a3169308f788
ashwin-sekar <ashwinsekar14@gmail.com>
2020-04-05 01:11:05 +0530

    Updates

---
+++ precache-manifest.574f34027897c5f89e0b81da4484236c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54954a45038ab58a3f56cd677ea2e9da",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "ab9fd98c1cdd174f6371",
+    "url": "/face-recognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c1b26a8812948057577e",
+    "url": "/face-recognition/static/css/main.cf531571.chunk.css"
+  },
+  {
+    "revision": "ab9fd98c1cdd174f6371",
+    "url": "/face-recognition/static/js/2.5437d822.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition/static/js/2.5437d822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b26a8812948057577e",
+    "url": "/face-recognition/static/js/main.1e82a2c1.chunk.js"
+  },
+  {
+    "revision": "930ff2fab396760df308",
+    "url": "/face-recognition/static/js/runtime-main.74281b39.js"
+  }
+]);
