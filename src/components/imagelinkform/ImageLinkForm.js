2a91c69e320271486ac14b917bb285ba6e42d464
ashwin-sekar <ashwinsekar14@gmail.com>
2020-04-05 02:40:55 +0530

    color changes

--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm= ({ onInputchange, onSubmit }) => {
            <div className='center'>
                <div className='form center pa4 br3 shadow-5'>
                 <input className='f4 pa2 w-70 center' type='text' onChange={ onInputchange } />
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer' onClick={ onSubmit }>Detect</button>
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg pointer' onClick={ onSubmit }>Detect</button>
                </div>
            </div>
             
